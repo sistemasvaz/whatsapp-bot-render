c3f7120f5694cc92644aae2165777f269bf6daca
Sistemas Vaz <sistemas@riobr.top>
2025-11-11 17:53:35 -0300

    Deploy Render com FAQ

--- gitup.bat
+++ gitup.bat
@@ -1,6 +1,7 @@
+echo "# whatsapp-bot-render" >> README.md
 git init
-git add .
-git commit -m "Deploy Render"
+git add README.md
+git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/sistemasvaz/whatsapp-bot-render.git
 git push -u origin main
